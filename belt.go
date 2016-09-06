354e7e72b61aa050f0259ab0f452c9be30683de9
Maxime Bury <maxime.bury@gmail.com>
2016-09-06 16:22:05 -0400

    Add belt.Contains + assert lib

--- belt.go
+++ belt.go
@@ -4,6 +4,7 @@ package belt
 import (
 	"log"
 	"os"
+	"reflect"
 )
 
 var (
@@ -21,4 +22,20 @@ func Check(err error) {
 
 		os.Exit(1)
 	}
+}
+
+// Contains returns true if x is an element of xs. Though it is not enforced,
+// x and the elements of xs must be of the same type for proper comparison.
+// This uses a slow linear search suitable for small amounts of data. Equality
+// between elements is tested using reflect.DeepEqual.
+func Contains(xs interface{}, x interface{}) bool {
+	if slice := reflect.ValueOf(xs); slice.Kind() == reflect.Slice {
+		for i := 0; i < slice.Len(); i++ {
+			if reflect.DeepEqual(x, slice.Index(i).Interface()) {
+				return true
+			}
+		}
+	}
+
+	return false
 }
--- belt_test.go
+++ belt_test.go
@@ -2,12 +2,15 @@ package belt_test
 
 import (
 	"errors"
+	"fmt"
+	"math/rand"
 	"net/http"
 	"os"
 	"os/exec"
 	"testing"
 
 	"github.com/Bowbaq/belt"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestCheckNil(t *testing.T) {
@@ -19,9 +22,7 @@ func TestCheckNil(t *testing.T) {
 	cmd := exec.Command(os.Args[0], "-test.run=TestCheckNil")
 	cmd.Env = append(os.Environ(), "TEST_CHECK_NIL=1")
 
-	if err := cmd.Run(); err != nil {
-		t.Fatalf("process ran with err %v, want exit status 0", err)
-	}
+	assert.NoError(t, cmd.Run())
 }
 
 func TestCheckErr(t *testing.T) {
@@ -32,15 +33,78 @@ func TestCheckErr(t *testing.T) {
 
 	cmd := exec.Command(os.Args[0], "-test.run=TestCheckErr")
 	cmd.Env = append(os.Environ(), "TEST_CHECK_ERR=1")
-	err := cmd.Run()
 
-	if e, ok := err.(*exec.ExitError); ok && !e.Success() {
-		return
+	if err := cmd.Run(); assert.IsType(t, err, &exec.ExitError{}) {
+		exit := err.(*exec.ExitError)
+		assert.False(t, exit.Success(), "Process ran without error, expected exit code 1")
 	}
-	t.Fatalf("process ran with err %v, want exit status 1", err)
 }
 
 func ExampleCheck() {
 	_, err := http.Get("http://www.example.com/")
 	belt.Check(err)
+}
+
+func TestContainsNilSlice(t *testing.T) {
+	assert.False(t, belt.Contains(nil, "needle"))
+}
+
+func TestContainsEmptySlice(t *testing.T) {
+	assert.False(t, belt.Contains([]string{}, "needle"))
+}
+
+func TestContainsPositiveFirst(t *testing.T) {
+	assert.True(t, belt.Contains([]int{1, 2, 3, 4, 5}, 1))
+}
+
+func TestContainsPositiveLast(t *testing.T) {
+	assert.True(t, belt.Contains([]int{1, 2, 3, 4, 5}, 5))
+}
+
+func TestContainsPositiveMiddle(t *testing.T) {
+	assert.True(t, belt.Contains([]int{1, 2, 3, 4, 5}, 3))
+}
+
+func TestContainsNegative(t *testing.T) {
+	assert.False(t, belt.Contains([]int{1, 2, 3, 4, 5}, 42))
+}
+
+func TestContainsNegativeTypeMismatch(t *testing.T) {
+	assert.False(t, belt.Contains([]int{1, 2, 3, 4, 5}, "42"))
+}
+
+func benchmarkContains(l int, b *testing.B) {
+	xs := make([]int, l)
+	for i := 0; i < l; i++ {
+		xs[i] = rand.Intn(1000)
+	}
+	b.ResetTimer()
+
+	for n := 0; n < b.N; n++ {
+		belt.Contains(xs, rand.Intn(1000))
+	}
+}
+
+func BenchmarkContains10(b *testing.B)    { benchmarkContains(10, b) }
+func BenchmarkContains100(b *testing.B)   { benchmarkContains(100, b) }
+func BenchmarkContains1000(b *testing.B)  { benchmarkContains(1000, b) }
+func BenchmarkContains10000(b *testing.B) { benchmarkContains(10000, b) }
+
+func ExampleContains() {
+	var cakes = []string{"Tiramisu", "Apple Crumble", "Blueberry Pie"}
+
+	if belt.Contains(cakes, "Tiramisu") {
+		fmt.Println("Tiramisu is a cake")
+	} else {
+		fmt.Println("Tiramisu is *not* a cake")
+	}
+
+	if belt.Contains(cakes, "Ice Cream") {
+		fmt.Println("Ice Cream is a cake")
+	} else {
+		fmt.Println("Ice Cream is *not* a cake")
+	}
+	// Output:
+	// Tiramisu is a cake
+	// Ice Cream is *not* a cake
 }
